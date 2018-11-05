826807903c102687e5f6401035b368d2e0ba9b2f
Karthik T <karthik@accern.com>
2018-11-05 14:32:21 -0500

    default value is production insetead of prod

--- setup.py
+++ setup.py
@@ -66,7 +66,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
     ]
